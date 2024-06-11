d325a6f0152fba455451ec01401be07a8be162a5
malik aziz awan <pvtmalikazizawan@gmail.com>
2024-06-11 18:45:17 +0500

    Completely rescponcive

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add('active-tab');
     event.currentTarget.classList.add('active-link');
 }
+
+// -------Menu for Mobile--------
+
+let sidemenu = document.getElementById('sidemenu');
+
+function openmenu() {
+    sidemenu.style.right = '0px';
+}
+
+function closemenu() {
+    sidemenu.style.right = '-200px';
+}
+
+
+// Contact data bas
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxKxdPIN_nZDZYI6EUbZoa7QlmCrBaOdN0y_lcaed7MWUSM2htgu7JNitYbw-B95ECT/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+})
