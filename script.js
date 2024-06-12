d1df40e2d60e9f9b142f01baacc6d45ee7afd3a8
KhanzaidGithub <zaidscestudent@gmail.com>
2024-06-11 20:36:49 -0700

    .

--- script.js
+++ script.js
@@ -32,10 +32,27 @@ function closemenu() {
 
 const scriptURL = ' https://script.google.com/macros/s/AKfycbxKv18IVqjU_FcG2-07U0gWuvN6e8pu-65SbCTFk4YxUK0cI5U4kkLwK7wfXi1qGW12/exec'
 const form = document.forms['submit-to-google-sheet']
-
+let msg = document.getElementById('msg');
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => console.log('Success!', response))
+        .then(response => {
+            msg.innerHTML = 'Message sent succesfully!';
+            setTimeout(() => {
+                msg.innerHTML = ''
+            }, 5000);
+            form.reset();
+        })
         .catch(error => console.error('Error!', error.message))
 })
+
+
+
+    // Typing CDN
+  
+
+
+    var typed = new Typed('#element', {
+        strings: ['Front-End Developer.', 'HTML,CSS,Bootstrap and Js.', 'Mern Stack.'],
+        typeSpeed: 50,
+    });
